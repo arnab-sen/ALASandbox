43551352970df331136da222c23fe50d83a376db
Arnab Sen <arnab.sen210@gmail.com>
2021-02-20 17:38:23 +1300

    The application now displays a simple window that opens a new popup window when the A key is pressed

--- ALASandbox/Application.cs
+++ ALASandbox/Application.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using Libraries;
 using ProgrammingParadigms;
 using DomainAbstractions;
@@ -40,11 +42,15 @@ namespace ALASandbox
             MainWindow mainWindow = new MainWindow(height:720,width:1280) {InstanceName="mainWindow"}; /* {"IsRoot":false} */
             Vertical mainWindowContents = new Vertical() {InstanceName="mainWindowContents"}; /* {"IsRoot":false} */
             UIConfig UIConfig_mainWindowContents = new UIConfig() {InstanceName="UIConfig_mainWindowContents"}; /* {"IsRoot":false} */
+            KeyEvent id_fd1697fb6a6948e58074e4764f33c12b = new KeyEvent(eventName:"KeyDown") {InstanceName="id_fd1697fb6a6948e58074e4764f33c12b",Key=Key.A}; /* {"IsRoot":false} */
+            PopupWindow id_727a2a63cddd49629a222ccedb6094fd = new PopupWindow() {InstanceName="id_727a2a63cddd49629a222ccedb6094fd",Height=500,Width=500,Resize=SizeToContent.Manual}; /* {"IsRoot":false} */
             // END AUTO-GENERATED INSTANTIATIONS FOR MainDiagram
 
             // BEGIN AUTO-GENERATED WIRING FOR MainDiagram
             mainWindow.WireTo(UIConfig_mainWindowContents, "iuiStructure"); /* {"SourceType":"MainWindow","SourceIsReference":false,"DestinationType":"UIConfig","DestinationIsReference":false,"Description":"","SourceGenerics":[],"DestinationGenerics":[]} */
             UIConfig_mainWindowContents.WireTo(mainWindowContents, "child"); /* {"SourceType":"UIConfig","SourceIsReference":false,"DestinationType":"Vertical","DestinationIsReference":false,"Description":"","SourceGenerics":[],"DestinationGenerics":[]} */
+            mainWindow.WireTo(id_fd1697fb6a6948e58074e4764f33c12b, "eventHandlers"); /* {"SourceType":"MainWindow","SourceIsReference":false,"DestinationType":"KeyEvent","DestinationIsReference":false,"Description":"","SourceGenerics":[],"DestinationGenerics":[]} */
+            id_fd1697fb6a6948e58074e4764f33c12b.WireTo(id_727a2a63cddd49629a222ccedb6094fd, "eventHappened"); /* {"SourceType":"KeyEvent","SourceIsReference":false,"DestinationType":"PopupWindow","DestinationIsReference":false,"Description":"","SourceGenerics":[],"DestinationGenerics":[]} */
             // END AUTO-GENERATED WIRING FOR MainDiagram
 
 
